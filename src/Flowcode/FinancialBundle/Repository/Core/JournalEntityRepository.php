43aa37cf6dbfb13d72c0b10c3dc1c49c0d61823f
juliansci <julian.scialabba@gmail.com>
2017-06-19 18:12:24 -0300

    Fix @fecha

--- src/Flowcode/FinancialBundle/Repository/Core/JournalEntityRepository.php
+++ src/Flowcode/FinancialBundle/Repository/Core/JournalEntityRepository.php
@@ -10,16 +10,6 @@ use Flowcode\FinancialBundle\Model\Core\AccountInterface;
  */
 class JournalEntityRepository extends EntityRepository
 {
-    /**
-     * Se envia una cuenta y debe dar para la misma debito menos credito de todos
-     * los movimientos que esta cuenta tiene.
-     * @fecha   2017-04-07
-     * @author Francisco Memoli Olmos
-     * @email   fmemoli@flowcode.com.ar
-     * @version [version]
-     * @param   AccountInterface        $account [description]
-     * @return  [number]                           [description]
-     */
     public function getBalance(AccountInterface $account)
     {
         $qb = $this->createQueryBuilder("a");
